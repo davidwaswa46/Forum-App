53e32e5049e3503e76e8e504bca7168ca5dac761
Waswa024 <sobradave@gmail.com>
2024-02-17 05:22:26 +0300

    commit on seeding

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User; 
 
 
 return new class extends Migration
@@ -12,9 +13,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts','personal_access_tokens', function (Blueprint $table) {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            /*$table->foreignIdFor(User ::class)->constrained()->restrictOnDelete();*/
+            $table->foreignIdFor(User ::class)->constrained()->restrictOnDelete();
             $table->morphs('tokenable');
             $table->string('name');
             $table->string('title');
